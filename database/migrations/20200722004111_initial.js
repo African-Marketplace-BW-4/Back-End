343818b88746cf914f50491878aaa094f598b384
tarik-84 <niceloveman200@yahoo.com>
2020-07-24 03:21:59 -0400

    deploy

--- database/migrations/20200722004111_initial.js
+++ database/migrations/20200722004111_initial.js
@@ -9,16 +9,25 @@ exports.up = async function(knex) {
       table.text('email').notNullable().unique()
       
   })
+  await knex.schema.createTable('locations', (table) => {
+    table.increments()
+    table.text('location').notNullable()
+})
   await knex.schema.createTable('items', (table) => {
       table.increments('id')
       table.text('name').notNullable()
       table.text('description').notNullable()
       table.decimal('price').notNullable()
-      table.text('location')
+      table.integer('location_id')
+           .references('id')
+           .inTable('locations')
+           .onDelete('CASCADE')
+           .onUpdate('CASCADE'); 
   })
 };
 
 exports.down = async function(knex) {
   await knex.schema.dropTableIfExists('items')
+  await knex.schema.dropTableIfExists('locations')
   await knex.schema.dropTableIfExists('users')
 };
--- database/seeds/01-items.js
+++ database/seeds/01-items.js
@@ -1,11 +1,11 @@
 
 exports.seed = async function(knex) {
   await knex('items').insert([
-    {name: 'eggs', description: 'organic, 12 eggs', price: 2.49, location_id: 'Michigan'},
-    {name: 'milk', description: 'whole milk, 1 liter', price: 3.99, location_id: 'Ohio'},
-    {name: 'beef', description: 'cow meat', price: 6.99, location_id: 'Illinois'},
-    {name: 'turkey', description: 'best turkey', price: 4.99, location_id: 'Michigan'},
-    {name: 'tomato', description: 'vine tomato', price: 1.89, location_id: 'Ohio'},
-    {name: 'onion', description: 'red onion', price: 1.59, location_id: 'Illinois'},
+    {name: 'eggs', description: 'organic, 12 eggs', price: 2.49, location_id: 1},
+    {name: 'milk', description: 'whole milk, 1 liter', price: 3.99, location_id: 1},
+    {name: 'beef', description: 'cow meat', price: 6.99, location_id: 2},
+    {name: 'turkey', description: 'best turkey', price: 4.99, location_id: 2},
+    {name: 'tomato', description: 'vine tomato', price: 1.89, location_id: 3},
+    {name: 'onion', description: 'red onion', price: 1.59, location_id: 3},
   ])
 };
