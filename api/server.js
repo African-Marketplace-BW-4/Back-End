29b17ddbb91a9982281f059cd1f0c9ed5defedca
tarik-84 <niceloveman200@yahoo.com>
2020-07-24 16:33:55 -0400

    deploy

--- api/server.js
+++ api/server.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
-const cookieParser = require('cookie-parser')
 const authRouter = require('../auth/auth-router');
 const itemsRouter = require('../items/items-router')
 const restrict = require('../auth/auth-middleware')
@@ -9,17 +8,13 @@ const restrict = require('../auth/auth-middleware')
 
 const server = express();
 server.use(helmet())
-// var corsOptions = {
-// 	origin: "http://localhost:3000/sign-up",
-// 	optionsSuccessStatus: 200,
-//   };
+
 const origin =
-process.env.JWT_SECRET === 'development' ? 'http://localhost:3000' : 'http://example.com';
+process.env.JWT_SECRET === 'production' ? 'http://localhost:3000' : 'http://example.com';
 server.use(cors({ credentials: true, origin }));
 
-// server.use(cors(corsOptions))
 server.use(express.json());
-server.use(cookieParser())
+
 
 server.use('/api', authRouter);
 server.use('/api/items', itemsRouter, restrict());
