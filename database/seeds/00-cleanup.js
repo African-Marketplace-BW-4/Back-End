4d7bcd4738dd9be01e6c047de4277bcf2163c420
Tarik Najim <58613365+tarik-84@users.noreply.github.com>
2020-07-23 22:35:24 -0400

    Update 00-cleanup.js

--- database/seeds/00-cleanup.js
+++ database/seeds/00-cleanup.js
@@ -1,5 +1,5 @@
 
 exports.seed = async function(knex) {
-  await knex.raw('TRUNCATE TABLE location CASCADE')
+  await knex.raw('TRUNCATE TABLE locations CASCADE')
   await knex.raw('TRUNCATE TABLE items CASCADE')
 };
