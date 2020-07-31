8720046d4ab641b5bf99ceeb8f67e1ecca7c65ba
tarik-84 <niceloveman200@yahoo.com>
2020-07-30 21:23:05 -0400

    MVP

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,6 +1,6 @@
 const supertest = require('supertest')
 const server = require('../api/server')
-const db = require('../data/dbConfig')
+const db = require('../database/dbConfig')
 
 beforeEach(async () => {
     await db.seed.run()
